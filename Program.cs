59d3e70066b54ea1781faa627cb8b7e9cd80a3e5
misbah ramadani <misbahramadhani1@gmail.com>
2022-09-16 17:47:03 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var client = new RestClient("http://dummy.restapiexample.com/api/v1");
+                var request = new RestRequest("employees", Method.GET);
+                var content = client.Execute(request).Content;
+                Console.WriteLine(content);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
